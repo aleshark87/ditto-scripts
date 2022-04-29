489a296c3bf7b39345a2f4e9fa7c4081ca62861d
Alessandro <trofui@gmail.com>
2022-04-29 12:28:14 +0200

    working

--- createThing.sh
+++ createThing.sh
@@ -1,5 +1,5 @@
 ./deletePolicy.sh
-curl --location --request PUT -u ditto:ditto 'http://localhost:8080/api/2/things/com.project.thesis:greenhouse01' \
+curl --location --request PUT -u ditto:ditto 'http://localhost:8080/api/2/things/com.project.thesis:greenhouse' \
 --header 'Content-Type: application/json' \
 --data-raw '{
     "definition": "https://raw.githubusercontent.com/aleshark87/WoTModels/main/greenhouse/greenhouse.tm.jsonld"
