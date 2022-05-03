c25d1c751228650b3d69ba1ecdc6b1a66d790f69
Alessandro <trofui@gmail.com>
2022-05-03 14:42:52 +0200

    new mapper for new feature

--- createConnection.sh
+++ createConnection.sh
@@ -1,1 +1,1 @@
-curl -X POST 'http://localhost:8080/devops/piggyback/connectivity?timeout=10' -u 'devops:foobar' -H 'Content-Type: application/json' -d @./mqtt_newmapping.json
+curl -X POST 'http://localhost:8080/devops/piggyback/connectivity?timeout=10' -u 'devops:foobar' -H 'Content-Type: application/json' -d @./mqtt_newnewMapping.json
--- javascriptMappers/newMapInbound.js
+++ javascriptMappers/newMapInbound.js
@@ -5,11 +5,14 @@ function mapToDittoProtocolMsg(headers, textPayload, bytePayload, contentType) {
     const pathTemperature = '/features/temperature/properties/';
     const pathHumidity = '/features/humidity/properties/';
     const pathBrightness = '/features/brightness/properties/';
+    const pathLight = '/features/light/properties/';
     const valueTemperature = { value: jsonData.temperature };
     const valueHumidity = { value: jsonData.humidity };
     const valueBrightness = { value: jsonData.brightness };
+    const valueLight = { value: jsonData.light };
     const dittoProtocolMsgTemperature = Ditto.buildDittoProtocolMsg(thingId[0], thingId[1], 'things', 'twin', 'commands', 'modify', pathTemperature, headers, valueTemperature);
     const dittoProtocolMsgHumidity = Ditto.buildDittoProtocolMsg(thingId[0], thingId[1], 'things', 'twin', 'commands', 'modify', pathHumidity, headers, valueHumidity);
     const dittoProtocolMsgBrightness = Ditto.buildDittoProtocolMsg(thingId[0], thingId[1], 'things', 'twin', 'commands', 'modify', pathBrightness, headers, valueBrightness);
-    const array = [dittoProtocolMsgTemperature, dittoProtocolMsgHumidity, dittoProtocolMsgBrightness];
+    const dittoProtocolMsgLight = Ditto.buildDittoProtocolMsg(thingId[0], thingId[1], 'things', 'twin', 'commands', 'modify', pathLight, headers, valueLight);
+    const array = [dittoProtocolMsgTemperature, dittoProtocolMsgHumidity, dittoProtocolMsgBrightness, dittoProtocolMsgLight];
 return array; }
